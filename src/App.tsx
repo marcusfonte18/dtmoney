6c0853a4e92a842cdcd13f6a2ca8f6b1e8a0331d
Marcus Vinicius <65238795+marcusfonte18@users.noreply.github.com>
2022-06-14 22:35:53 -0300

    projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react';
-import Modal from 'react-modal'
+import Modal from 'react-modal';
 
 import { Dashboard } from './src/components/Dashboard';
 import { Header } from './src/components/Header/Index';
 import { NewTransactionModal } from './src/components/NewTransactionModal';
+import { TransactionsProvider } from './src/hooks/useTransactions';
 import { GlobalStyle } from './src/styles/global';
+
 
 Modal.setAppElement('#root')
 
 export function App() {
 
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+
 
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true)
@@ -22,7 +26,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -33,7 +37,7 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </ TransactionsProvider>
   );
 }
 
--- src/src/components/TransactionsTable/index.tsx
+++ src/src/components/TransactionsTable/index.tsx
@@ -1,54 +1,41 @@
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
 import { Container } from './styles';
 
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createAt: string;
-}
+
 
 export function TransactionTable() {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
+  const { transactions } = useTransactions()
 
-    useEffect(() => {
-        api.get('transactions')
-            .then((response => setTransactions(response.data.transactions)))
-    }, [])
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
 
-    return (
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+        <tbody>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td> {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createAt))}</td>
+            </tr>
 
-                <tbody>
-                    {transactions.map(transaction => (
-                        <tr key={transaction.id}>
-                            <td>{transaction.title}</td>
-                            <td className={transaction.type}>
-                                {new Intl.NumberFormat('pt-BR', {
-                                    style: 'currency',
-                                    currency: 'BRL'
-                                }).format(transaction.amount)}
-                            </td>
-                            <td>{transaction.category}</td>
-                            <td> {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createAt))}</td>
-                        </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
 
-                    ))}
-                </tbody>
-            </table>
-        </Container>
-
-    )
+  )
 }
